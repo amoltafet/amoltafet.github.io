e365930ba15c28b3b0b66986ce562aca71c9c36e
Ahmad Moltafet <amoltafet@zagmail.gonzaga.edu>
2022-07-14 11:08:33 -0700

    Updates

---
+++ precache-manifest.ac67cc937ab076e978682ae27abbf2de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc36f80f6e17c1005ccd3bbee214f6d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d0b08b9b765697b6f63",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "76a1df8660264dcd0cdc",
+    "url": "/static/js/2.98199f8a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.98199f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0b08b9b765697b6f63",
+    "url": "/static/js/main.192876fc.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "c66eaa336c8c7601206abbb5288cb421",
+    "url": "/static/media/ahmad.c66eaa33.jpeg"
+  },
+  {
+    "revision": "d19639556e5b9028d99e9b83dbe3c790",
+    "url": "/static/media/gonzaga.d1963955.jpeg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "907ab814036b3d80d3c5c1b579f0cad6",
+    "url": "/static/media/intellitect.907ab814.jpeg"
+  },
+  {
+    "revision": "bdd9d78f69aa2d98d9268d6ab6768b6a",
+    "url": "/static/media/nwc.bdd9d78f.png"
+  },
+  {
+    "revision": "ce5763b7e9d352f69bd11519a749e17a",
+    "url": "/static/media/resume.ce5763b7.pdf"
+  },
+  {
+    "revision": "f8083c4486a4030d088ab2ae0d726d0e",
+    "url": "/static/media/synergy.f8083c44.png"
+  }
+]);
